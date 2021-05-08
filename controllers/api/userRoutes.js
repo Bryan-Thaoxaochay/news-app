1888af6d7f0d9927bca18bf9d54d0e9f413454ab
Jim Nguyen <jimnguyen220@gmail.com>
2021-05-08 13:52:18 -0500

    commented out session storage for now and homeRoutes, added index.js files to different folders

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/User');
 
 // Rendering Login Page
 router.get('/login', async (req, res) => {
--- server.js
+++ server.js
@@ -10,18 +10,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
 
-app.use(session(sess));
-app.use(compression());
+// app.use(session(sess));
+// app.use(compression());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
