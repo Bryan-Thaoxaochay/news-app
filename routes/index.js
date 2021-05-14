3a0693e2d027c15bb5ef40fad963e65e9fd07af1
LittleMagician <bthaoxaochay@gmail.com>
2021-05-14 10:53:19 -0500

    Updated routes. Server starts, but no routes yet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+const path = require('path');
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes');
 
-// router.use('/', homeRoutes);
+// Routes
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
+// Send React App
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 module.exports = router;
