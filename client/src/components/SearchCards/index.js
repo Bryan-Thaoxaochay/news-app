ab89d2b0fdf08a126b57e21bff787948f76e1c41
BeeNorrg <brandon.n.norrgard@gmail.com>
2021-05-20 18:59:40 -0500

    added more sub-components to searchCards, added ArticleCard which is currently empty

--- client/src/components/SearchCards/index.js
+++ client/src/components/SearchCards/index.js
@@ -1,16 +1,49 @@
 import React from "react";
+import {Container } from "../components/Grid";
+import "./style.css";
 
 
 export function SourceView(props){
     return (
-    <div className={"card articleCard sourceView indexUp" + props.className}></div>
+    <div className={"card articleCard sourceView indexUp" + props.className}>
+        <Container fluid className="sourceSearch">
+            <div className="list-group" id="">
+                
+            </div>
+            <div data-bs-spy="scroll" data-bs-target="" data-bs-offset="0" class="chScroll" tabindex="0">
+                <SourceBtn/>
+                <SourceCollapse>
+
+                </SourceCollapse>
+            </div>
+        </Container>
+    </div>
     )
 };
 
+function SourceBtn(props){
+    return(
+    <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="" aria-expanded="false" aria-controls=""></button>
+    )
+}
+
+function SourceCollapse(props){
+    return (
+    <div class="collapse" id="">
+        <div class="card card-body">
+        </div>
+    </div>
+    )
+}
 
 
 export function FullView(props){
     return (
-    <div className={"card articleCard sourceView indexDown" + props.className}></div>
+    <div className={"card articleCard fullView indexDown" + props.className}>
+       
+    </div>
     )
 }
+
+
+
