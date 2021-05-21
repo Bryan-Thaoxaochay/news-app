e91fd84b0e818aad2b498b282a439aed872605ee
Calvin Jacobsen <Jaco1932@umn.edu>
2021-05-20 19:21:11 -0500

    used mapping to generated left sidebar

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ArticleCategory from "../../utils/ArticleCategory";
 import API from "../../utils/API"
+import "./style.css"
 
 function SideBar() {
 
-    const [category, setCategory] = useState({
-        category: ""
-    });
+    const categories = ["Tech", "Business", "General", "Health", "Science", "Entertainment", "Sports"]
+    const [category, setCategory] = useState(categories);
 
 
     function handleOnClick(selected) {
@@ -16,88 +16,31 @@ function SideBar() {
 
     useEffect(() => {
         API.getCategory(category)
-        .then(res =>{
-            console.log(res.data)
-        });
+            .then(res => {
+                console.log(res.data)
+            });
         console.log(category)
     })
 
 
-  ///Not sure if we want to do the Provider/context thing or not?  Maybe we just make API calls directly from this page instead?
+    ///Not sure if we want to do the Provider/context thing or not?  Maybe we just make API calls directly from this page instead?
     return (
         <ArticleCategory.Provider value={category}>
             <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ "width": '280px' }}>
                 <ul>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={()=>handleOnClick("technology")}
-                        >
-                            Tech
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={() => handleOnClick("business")}
-                        >
-                            Business
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={() => handleOnClick("general")}
-                        >
-                            General
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={() => handleOnClick("health")}
-                        >
-                            Health
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={() => handleOnClick("science")}
-                        >
-                            Science
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={() => handleOnClick("entertainment")}
-                        >
-                            Entertainment
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            type="button"
-                            className="btn btn-link"
-                            value="technology"
-                            onClick={() => handleOnClick("sports")}
-                        >
-                            Sports
-                        </button>
-                    </li>
+                    {category.map(categoryElement => (
+                        <li className="categorySearch" >
+                            <button
+                                type="button"
+                                className="btn btn-link"
+                                id={categoryElement}
+                                value="technology"
+                                onClick={() => handleOnClick("technology")}
+                            >
+                                {categoryElement}
+                            </button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </ArticleCategory.Provider>
