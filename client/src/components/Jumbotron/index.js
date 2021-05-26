ad67fec564789201aeb6ba2f1ad76fdbf9231e44
Jim Nguyen <jimnguyen220@gmail.com>
2021-05-26 18:46:34 -0500

    updated Articles result to match search results

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,8 +4,8 @@ function Jumbotron({ children }) {
   return (
     //added minHeight to allow for jumbotron to resize as articles are added
     <div
-      style={{ minHeight: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron border border-success"
+      style={{ minHeight: 50, clear: "both", paddingTop: 20, textAlign: "center" }}
+      className="jumbotron border border-dark"
     >
       {children}
     </div>
